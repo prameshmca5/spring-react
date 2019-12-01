314e4abda7ee571784db603958230f8929a163d4
Ramesh Pongiannan <ramesh.pongiannan@bestinet.com.my>
2019-12-01 23:26:01 +0800

    Added Component for Router using and Login

--- src/App.js
+++ src/App.js
@@ -1,27 +1,16 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+
 import './App.css';
-import FirstComp from "./components/FirstComp";
-import SecondComp from "./components/SecondComp";
-import Counter from "./components/counter/Counter";
-
+import TodoApp from "./components/todo/todoApp";
 class App extends Component{
-  render() {
-    return (
-        <div className="App">
-          <header className="App-header">
-              <h2>Create and display for Application Header </h2>
-              <Counter byinc={1}></Counter>
-              <Counter byinc={-10}></Counter>
-              <Counter byinc={100}></Counter>
-
-          </header>
-        </div>
-    );
-  }
+    render() {
+        return (
+            <div className="App">
+                <TodoApp></TodoApp>
+            </div>
+        );
+    }
 }
-
-
 
 export default App;
 
